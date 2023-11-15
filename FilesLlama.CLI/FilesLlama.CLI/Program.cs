13122856f1d8fe19eb390b1843c196166c9f7c7d
Zarrko <zacmwangi60@gmail.com>
2023-11-15 13:31:03 +0300

    setup

---
+++ FilesLlama.CLI/FilesLlama.CLI/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+var services = new ServiceCollection();
+
+// Add Redis
+services.AddSingleton<IConnectionMultiplexer>(_=> ConnectionMultiplexer.Connect("localhost"));
